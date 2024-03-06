0d9f5ee6fa8d9e29115f721d9d9e3960d11ee50b
darriola2021670 <darriola-2021670@kinal.edu.gt>
2024-03-05 23:30:58 -0600

    se hicieron cambios en el codigo debido a errores

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ config();
 
 import Server from "./config/server.js";
 
-// Crear una instancia del servidor
 const server = new Server();
 
-// Iniciar el servidor
 server.listen();
--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -6,7 +6,6 @@ export const login = async (req, res) => {
     const { correo, password } = req.body;
 
   try {
-    //verificar si el email existe:
     const usuario = await Usuario.findOne({ correo });
 
     if (!usuario) {
@@ -14,20 +13,17 @@ export const login = async (req, res) => {
         msg: "Credenciales incorrectas, Correo no existe en la base de datos",
       });
     }
-    //verificar si el ususario está activo
     if (!usuario.estado) {
       return res.status(400).json({
         msg: "El usuario no existe en la base de datos",
       });
     }
-    // verificar la contraseña
     const validPassword = bcryptjs.compareSync(password, usuario.password);
     if (!validPassword) {
       return res.status(400).json({
         msg: "La contraseña es incorrecta",
       });
     }
-    //generar el JWT
     const token = await generarJWT( usuario.id);
 
     res.status(200).json({
